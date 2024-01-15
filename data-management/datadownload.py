32aa2364721df1968a11cda7ddad4849a5a56233
Deepak George <deepakmgsu@gmail.com>
2024-01-15 12:52:47 -0500

    downloaded the data dictionary json file

--- data-management/datadownload.py
+++ data-management/datadownload.py
@@ -1,3 +1,3 @@
 import urllib.request
-#urllib.request.urlretrieve("https://data.cdc.gov/api/views/9j2v-jamp/rows.csv?accessType=DOWNLOAD", "/Users/deepak/deathrates/data/raw/deathrates.csv")
+urllib.request.urlretrieve("https://data.cdc.gov/api/views/9j2v-jamp/rows.csv?accessType=DOWNLOAD", "/Users/deepak/deathrates/data/raw/deathrates.csv")
 urllib.request.urlretrieve("https://data.cdc.gov/api/views/9j2v-jamp/rows.json?accessType=DOWNLOAD", "/Users/deepak/deathrates/data/raw/metadata.json")
